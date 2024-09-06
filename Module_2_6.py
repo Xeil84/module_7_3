939e5bae370b549909c9cff4b9d0d32a29cd6b74
Xeil84 <avtotgl@gmail.com>
2024-09-06 10:06:57 +0400

    module_2_6

---
+++ Module_2_6.py
@@ -0,0 +1,15 @@
+n = int(input('Здравствуйте! Введите число от 3 до 20:'))
+q = []
+for i in range(1, n + 1):
+    one = []
+    one.append(i)
+    for j in range(0, n + 1):
+        two = []
+        two.append(j)
+        if (n % (i + j)) == 0:
+            if i == j or j == n or i == n or j < i:
+                continue
+            else:
+                q.append(i)
+                q.append(j)
+print(q)
