927e5e71d6f7475715fdafa6a01bbece9e4a5563
Xeil84 <avtotgl@gmail.com>
2024-10-22 22:39:11 +0400

    module_7_4

---
+++ Module_7/module_7_5.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+directory = r'C:\Users\User\PycharmProjects\pythonProject\Module_7'
+for root, dirs, files in os.walk(directory):
+
+    for file in files:
+        filepath = os.path.join(file)
+
+        filetime = os.stat(file).st_mtime
+
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        filesize = os.path.getsize(file)
+
+        parent_dir = os.path.dirname(file)
+
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
