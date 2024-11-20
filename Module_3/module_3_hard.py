435be0eede5db6174d940e32ca3a5a5232329d47
Xeil84 <avtotgl@gmail.com>
2024-11-20 12:40:49 +0400

    module_7_1_5

---
+++ Module_3/module_3_hard.py
@@ -0,0 +1,25 @@
+def calculate_sum(*args):
+    total_sum = 0
+
+    for arg in args:
+        if isinstance(arg, int):
+            total_sum += arg
+        elif isinstance(arg, str):
+            total_sum += len(arg)
+        elif isinstance(arg, (list, tuple, set)):
+            total_sum += calculate_sum(*arg)
+        elif isinstance(arg, dict):
+            total_sum += calculate_sum(*arg.keys(), *arg.values())
+
+    return total_sum
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_sum(data_structure)
+print(result)
