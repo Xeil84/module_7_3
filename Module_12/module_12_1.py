bb1e03bb9aebda2c8b901248bee2a97c45d5f0dc
Xeil84 <avtotgl@gmail.com>
2024-12-13 15:49:27 +0400

    module_7_1_6

---
+++ Module_12/module_12_1.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest import TestCase
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        walker = Runner('Name')
+        for i in range(10):
+            walker.walk()
+        self.assertEqual(walker.distance, 50)
+
+    def test_run(self):
+        runner = Runner('Name')
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+    def test_chalenge(self):
+        runner = Runner('Name')
+        walker = Runner('Surname')
+        for i in range(100):
+            runner.run()
+            walker.walk()
+        self.assertNotEqual(walker.distance, runner.distance)
+
+if __name__ == '__main__':
+    RunnerTest()
+    
